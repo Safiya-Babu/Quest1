7735921ecf34dece9529cf4f611c554f811ff6e0
Safiya Babu <safiyababu11@gmail.com>
2026-06-02 16:31:53 +0530

    Added Ollama streaming token-by-token response

---
+++ Ollama Quickstart/ollama.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def get_response(prompt):
+    url = "http://localhost:11434/api/generate"
+
+    data = {
+        "model": "llama3.1:8b",
+        "prompt": prompt,
+        "stream": True
+    }
+
+    response = requests.post(url, json=data, stream=True)
+
+    for line in response.iter_lines():
+        if line:
+            result = json.loads(line)
+            print(result["response"])
+
+prompt = ""
+
+while prompt != "exit":
+    prompt = input(">>> ")
+
+    if prompt != "exit":
+        get_response(prompt)
